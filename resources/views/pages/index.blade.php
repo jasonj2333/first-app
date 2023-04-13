074a48be147e408fa711fb2d012adffd7dad67d8
Jerzy Jasonek <jasonj2333@gmail.com>
2023-04-13 07:49:59 +0200

    Start school-project

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spis stron</title>
+</head>
+<body>
+    <h1>Spis treści</h1>
+</body>
+</html>
